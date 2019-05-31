ebeecf01f48c80a0bee72500c46f46d18587030e
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-31 14:44:32 -0700

    Stored dummy data in items.js for testing

---
+++ test/items.js
@@ -0,0 +1,107 @@
+const items = [
+    {
+        'itemId': '5a9689f-590d3ff-7857d9b-b5bbf59',
+        'lastUpdate': 1557100802,
+        'item': {
+            'name': 'The Milano',
+            'description': 'Guard the galaxy in style.',
+            'cost': 1200,
+            'type': 'glider',
+            'rarity': 'marvel',
+            'upcoming': false,
+            'images': {
+                'icon': 'https://fortnite-public-files.theapinetwork.com/glider/9df1b706eabbb63ff83fc02e4904d394.png',
+                'featured': 'https://fortnite-public-files.theapinetwork.com/featured/5a9689f-590d3ff-7857d9b-b5bbf59.png',
+                'background': 'https://fortnite-public-files.theapinetwork.com/image/5a9689f-590d3ff-7857d9b-b5bbf59.png',
+                'information': 'https://fortnite-public-files.theapinetwork.com/image/5a9689f-590d3ff-7857d9b-b5bbf59/item.png'
+            },
+            'obtained': '1200',
+            'obtained_type': 'vbucks',
+            'ratings': {
+                'avgStars': 4.48,
+                'totalPoints': 1174,
+                'numberVotes': 262
+            }
+        }
+    },
+    
+    {
+        'itemId': 'f421389-fe2a512-f780fc9-2e78757',
+        'lastUpdate': 1557100801,
+        'item': {
+            'name': 'Guardian Axe',
+            'description': 'Cut down cosmic threats.',
+            'cost': 500,
+            'type': 'pickaxe',
+            'rarity': 'marvel',
+            'upcoming': false,
+            'images': {
+                'icon': 'https://fortnite-public-files.theapinetwork.com/pickaxe/ebd2802ccdb7b8851a0bff5ad53d0776.png',
+                'featured': 'https://fortnite-public-files.theapinetwork.com/featured/f421389-fe2a512-f780fc9-2e78757.png',
+                'background': 'https://fortnite-public-files.theapinetwork.com/image/f421389-fe2a512-f780fc9-2e78757.png',
+                'information': 'https://fortnite-public-files.theapinetwork.com/image/f421389-fe2a512-f780fc9-2e78757/item.png'
+            },
+            'obtained': '500',
+            'obtained_type': 'vbucks',
+            'ratings': {
+                'avgStars': 3.85,
+                'totalPoints': 600,
+                'numberVotes': 156
+            }
+        }
+    },
+    
+    {
+        'itemId': '9537ea4-2e798a1-6557b1e-2366d78',
+        'lastUpdate': 1557100802,
+        'item': {
+            'name': `Widow's Bite`,
+            'description': 'Shockingly effective.',
+            'cost': 800,
+            'type': 'pickaxe',
+            'rarity': 'marvel',
+            'upcoming': false,
+            'images': {
+                'icon': 'https://fortnite-public-files.theapinetwork.com/pickaxe/8596f4f7d4e2a96ab478a753b246b576.png',
+                'featured': 'https://fortnite-public-files.theapinetwork.com/featured/9537ea4-2e798a1-6557b1e-2366d78.png',
+                'background': 'https://fortnite-public-files.theapinetwork.com/image/9537ea4-2e798a1-6557b1e-2366d78.png',
+                'information': 'https://fortnite-public-files.theapinetwork.com/image/9537ea4-2e798a1-6557b1e-2366d78/item.png'
+            },
+            'obtained': '800',
+            'obtained_type': 'vbucks',
+            'ratings': {
+                'avgStars': 4.42,
+                'totalPoints': 1334,
+                'numberVotes': 302
+            }
+        }
+    },
+    
+    {
+        'itemId': 'd54bbb3-089ec26-0c67700-5484644',
+        'lastUpdate': 1557100802,
+        'item': {
+            'name': 'Dance Off',
+            'description': 'Star-Lord&acirc;&euro;&trade;s perfect distraction.',
+            'cost': 500,
+            'type': 'emote',
+            'rarity': 'marvel',
+            'upcoming': false,
+            'images': {
+                'icon': 'https://fortnite-public-files.theapinetwork.com/emote/e4c7b4af53684e2f196ffb82d2ea659a.png',
+                'featured': null,
+                'background': 'https://fortnite-public-files.theapinetwork.com/image/d54bbb3-089ec26-0c67700-5484644.png',
+                'information': 'https://fortnite-public-files.theapinetwork.com/image/d54bbb3-089ec26-0c67700-5484644/item.png'
+            },
+            'obtained': '500',
+            'obtained_type': 'vbucks',
+            'ratings': {
+                'avgStars': 4.15,
+                'totalPoints': 1639,
+                'numberVotes': 395
+            }
+        }
+    }
+];
+
+export default items;
