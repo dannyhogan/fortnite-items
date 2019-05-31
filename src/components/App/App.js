e67896ee23170a11effa9b352083c9aee894df47
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-31 14:14:39 -0700

    Moved app component into separate folder

---
+++ src/components/App/App.js
@@ -0,0 +1,25 @@
+import Component from '../Component.js';
+import Header from '../Header/Header.js';
+
+class App extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+        const main = dom.querySelector('main');
+
+        const header = new Header();
+        dom.insertBefore(header.render(), main);
+
+        return dom;
+    }
+
+    renderTemplate() {
+        return /*html*/`
+            <div>
+                <main></main>
+            </div>
+        `;
+    }
+}
+
+export default App;
