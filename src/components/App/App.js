30e90502d02cda0d3d17fbd01e223c8c2084d3cd
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-02 17:02:42 -0700

    imported modal and api intp app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,9 @@
 import Component from '../Component.js';
 import Header from '../Header/Header.js';
+import ItemList from '../ItemList/ItemList.js';
+import ItemModal from '../ItemModal/ItemModal.js';
+import api from '../../services/fortnite-api.js';
+// import items from '../../../test/items.js';
 
 class App extends Component {
 
@@ -9,6 +13,25 @@ class App extends Component {
 
         const header = new Header();
         dom.insertBefore(header.render(), main);
+
+        const itemModal = new ItemModal({ clickedItem: {}, visible: false });
+        main.appendChild(itemModal.render());
+
+        let clickedItem;
+        const itemList = new ItemList({ 
+            data: [],
+            onClick: item => {
+                clickedItem = item;
+                itemModal.update({ clickedItem, visible: true });
+            }
+        });
+        main.appendChild(itemList.render());
+
+        api.getItems() 
+            .then(items => {
+                const data = items.data;
+                itemList.update({ data });
+            });
 
         return dom;
     }
